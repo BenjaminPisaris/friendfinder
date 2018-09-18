b62fae826170709c286102034c3acc72fce1f6eb
Ben Pisaris <datcrisis@gmail.com>
2018-09-18 13:03:24 -0400

    finished API routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,12 +3,12 @@ var friends = require("friends");
 
 //Routing
 //JSON the friends array from friends.js when the API is called
-app.get("/api/friends", function(req, res) {
+app.get("/api/friends", function (req, res) {
     res.json(friends);
-  });
+});
 
 //POST request for when a survey gets submitteds
-app.post("/api/friends", function(req, res){
+app.post("/api/friends", function (req, res) {
 
     //Create a placeholder object for the closest match
     var bestFriend = {
@@ -22,19 +22,32 @@ app.post("/api/friends", function(req, res){
     var userScores = userData.scores;
 
     //Create a variable that will be used to compare userScores with every other users score
-    var Difference;
+    var difference;
 
     //Loop through all possible users
-    
+    for (var i = 0; i < friends.length; i++) {
         //Inside the loop, make another loop that goes through each users score
-
-            //compare the absolute difference of each score
-        
-        //If the sum of the scores differences is less than the current closest score
-        //make that your new bestFriend
-
+        var friend = friends[i];
+        difference = 0;
+        //log friend.name for troubleshooting
+        console.log(friend.name);
+        //compare the absolute difference of each score
+        for (x = 0; x < friend.scores.length; x++) {
+            var friendScore = friend.scores[x];
+            var myScore = userScores[x];
+            difference += Math.abs(parseInt(myScore) - parseInt(friendScore));
+            //If the sum of the scores differences is less than the current closest score
+            if (difference < bestFriend.scoreDiff) {
+                //make that your new bestFriend
+                bestFriend.name = friend.name;
+                bestFriend.photo = friend.photo;
+                scoreDiff = difference;
+            }
+            
+        }
+    }
     //push userData to the friends array
-
+    friends.push(userData)
     //return a JSON with the users best match
-
+    res.json(bestFriend);
 });
