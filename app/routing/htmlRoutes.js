c72f326b54c15a3febd452c0a9ec47d540022b4f
Ben Pisaris <datcrisis@gmail.com>
2018-09-18 09:58:28 -0400

    added html routing

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+//Link Dependencies
+var path = require("path");
+
+module.exports = function(app) {
+    //HTML get requests
+    // the path /survey should load survey.html, anything else should redirect to home
+    app.get("/survey", function(requests, results) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    app.get("*", function (requests, results) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"))
+    });
+}
